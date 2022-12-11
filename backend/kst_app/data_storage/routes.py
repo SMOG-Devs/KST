f5de717e286f887b25089f188b5d7a54b3a7f2cc
YgLK <kuba21316@wp.pl>
2022-12-11 15:54:44 +0100

    Move data CRUD from routing to models.

--- backend/kst_app/data_storage/routes.py
+++ backend/kst_app/data_storage/routes.py
@@ -1,6 +1,5 @@
 from flask import request, Blueprint
-from kst_app import db, app
-from kst_app.data_storage.models import Sensor, Measurement, PredictedMeasurement, WeatherMeasurement
+from kst_app.data_storage.models import *
 
 data_storage = Blueprint('data_storage', __name__)
 
@@ -11,288 +10,211 @@ def hello():
     return "Hello world!"
 
 
-#       Sensors
+#      Sensor
 
-def format_sensor(sensor):
-    return {
-        "id": sensor.id,
-        "longitude": sensor.longitude,
-        "latitude": sensor.latitude,
-        "created_at": sensor.created_at,
-    }
-
-
-# create a sensor
+# add new sensor
 @data_storage.route('/sensor', methods=['POST'])
-def create_sensor():
-    longitude = request.json['longitude']
-    latitude = request.json['latitude']
-    sensor = Sensor(longitude, latitude)
-    db.session.add(sensor)
-    db.session.commit()
-    return format_sensor(sensor)
+def add_sensor_route():
+    added_sensor = add_sensor(
+        request.json.get('longitude'),
+        request.json.get('latitude')
+    )
+    return {"added_sensor": added_sensor.to_json()}
 
 
 # get all sensors
 @data_storage.route('/sensors', methods=['GET'])
-def get_sensors():
-    sensors = Sensor.query.order_by(Sensor.id.asc()).all()
-    sensors_list = []
-    for sensor in sensors:
-        sensors_list.append(format_sensor(sensor))
-    return {"sensors": sensors_list}
+def get_all_sensors_route():
+    sensors = get_all_sensors()
+    return {"sensors": list(map(lambda sensor: sensor.to_json(), sensors))}
 
 
-# get single sensor
-@data_storage.route('/sensor/<id>', methods=['GET'])
-def get_sensor(id):
-    sensor = Sensor.query.filter_by(id=id).one()
-    formatted_sensor = format_sensor(sensor)
-    return {"sensor": formatted_sensor}
+# get sensor by id
+@data_storage.route('/sensor/<sensor_id>', methods=['GET'])
+def get_sensor_route(sensor_id: int):
+    sensor = get_sensor(sensor_id)
+    return {"sensor": sensor.to_json()}
 
 
-# delete sensor
-@data_storage.route('/sensor/<id>', methods=['DELETE'])
-def delete_sensor(id):
-    sensor = Sensor.query.filter_by(id=id).one()
-    db.session.delete(sensor)
-    db.session.commit()
-    return f'Sensor (id {id}) deleted!'
+# delete sensor by id
+@data_storage.route('/sensor/<sensor_id>', methods=['DELETE'])
+def delete_sensor_route(sensor_id: int):
+    sensor = delete_sensor(sensor_id)
+    return {"deleted_sensor": sensor.to_json()}
 
 
-# update sensor
-@data_storage.route('/sensor/<id>', methods=['PUT'])
-def update_sensor(id):
-    sensor = Sensor.query.filter_by(id=id)
-    longitude = request.json['longitude']
-    latitude = request.json['latitude']
-    sensor.update(dict(longitude=longitude, latitude=latitude))
-    db.session.commit()
-    return {'sensor': format_sensor(sensor.one())}
-
-
-#       Measurement
-def format_measurement(measurement):
-    return {
-        "id": measurement.id,
-        "pm1": measurement.pm1,
-        "pm25": measurement.pm25,
-        "pm10": measurement.pm10,
-        "pressure": measurement.pressure,
-        "humidity": measurement.humidity,
-        "temperature": measurement.temperature,
-        "date": measurement.date,
-        "sensor_id": measurement.sensor_id,
-    }
+# update sensor by id
+@data_storage.route('/sensor/<sensor_id>', methods=['PUT'])
+def update_sensor_route(sensor_id: int):
+    updated_sensor = update_sensor(
+        sensor_id,
+        request.json.get('longitude'),
+        request.json.get('latitude')
+    )
+    return {"updated_sensor": updated_sensor.to_json()}
 
 
 # create a measurement
 @data_storage.route('/measurement', methods=['POST'])
-def create_measurement():
-    pm1 = request.json['pm1']
-    pm25 = request.json['pm25']
-    pm10 = request.json['pm10']
-    pressure = request.json['pressure']
-    humidity = request.json['humidity']
-    temperature = request.json['temperature']
-    date = request.json['date']
-    sensor_id = request.json['sensor_id']
-    measurement = Measurement(pm1, pm25, pm10, pressure, humidity, temperature, date, sensor_id)
-    db.session.add(measurement)
-    db.session.commit()
-    return format_measurement(measurement)
+def add_measurement_route():
+    measurement = add_measurement(
+        request.json.get('pm1'),
+        request.json.get('pm25'),
+        request.json.get('pm10'),
+        request.json.get('pressure'),
+        request.json.get('humidity'),
+        request.json.get('temperature'),
+        request.json.get('date'),
+        request.json.get('sensor_id')
+    )
+    return {"added_measurement": measurement.to_json()}
 
 
 # get all measurements
 @data_storage.route('/measurements', methods=['GET'])
-def get_measurements():
-    measurements = Measurement.query.order_by(Measurement.id.asc()).all()
-    measurements_list = []
-    for measurement in measurements:
-        measurements_list.append(format_measurement(measurement))
-    return {"measurements": measurements_list}
+def get_measurements_route():
+    measurements = get_all_measurements()
+    return {"measurements": list(map(lambda measurement: measurement.to_json(), measurements))}
 
 
-# get single measurement
-@data_storage.route('/measurement/<id>', methods=['GET'])
-def get_measurement(id):
-    measurement = Measurement.query.filter_by(id=id).one()
-    formatted_measurement = format_measurement(measurement)
-    return {"measurement": formatted_measurement}
+# get measurement by id
+@data_storage.route('/measurement/<measurement_id>', methods=['GET'])
+def get_measurement_route(measurement_id: int):
+    measurement = get_measurement(measurement_id)
+    return {"measurement": measurement.to_json()}
 
 
-# delete measurement
-@data_storage.route('/measurement/<id>', methods=['DELETE'])
-def delete_measurement(id):
-    measurement = Measurement.query.filter_by(id=id).one()
-    db.session.delete(measurement)
-    db.session.commit()
-    return f'Measurement (id {id}) deleted!'
+# delete measurement by id
+@data_storage.route('/measurement/<measurement_id>', methods=['DELETE'])
+def delete_measurement_route(measurement_id: int):
+    measurement = delete_measurement(measurement_id)
+    return {"deleted_measurement": measurement.to_json()}
 
 
-# update measurement
-@data_storage.route('/measurement/<id>', methods=['PUT'])
-def update_measurement(id):
-    measurement = Measurement.query.filter_by(id=id)
-    pm1 = request.json['pm1']
-    pm25 = request.json['pm25']
-    pm10 = request.json['pm10']
-    pressure = request.json['pressure']
-    humidity = request.json['humidity']
-    temperature = request.json['temperature']
-    date = request.json['date']
-    sensor_id = request.json['sensor_id']
-    measurement.update(
-        dict(pm1=pm1, pm25=pm25, pm10=pm10, pressure=pressure, humidity=humidity, temperature=temperature, date=date,
-             sensor_id=sensor_id))
-    db.session.commit()
-    return {'measurement': format_measurement(measurement.one())}
+# update measurement by id
+@data_storage.route('/measurement/<measurement_id>', methods=['PUT'])
+def update_measurement_route(measurement_id: int):
+    updated_measurement = update_measurement(
+        measurement_id,
+        request.json.get('pm1'),
+        request.json.get('pm25'),
+        request.json.get('pm10'),
+        request.json.get('pressure'),
+        request.json.get('humidity'),
+        request.json.get('temperature'),
+        request.json.get('date'),
+        request.json.get('sensor_id')
+    )
+    return {"updated_measurement": updated_measurement.to_json()}
 
 
-#   Predicted measurement
+#       Predicted Measurement
 
-def format_predicted_measurement(predicted_measurement):
-    return {
-        "id": predicted_measurement.id,
-        "pm1": predicted_measurement.pm1,
-        "pm25": predicted_measurement.pm25,
-        "pm10": predicted_measurement.pm10,
-        "date": predicted_measurement.date,
-        "sensor_id": predicted_measurement.sensor_id,
-    }
-
-
-# create a measurement
+# add a measurement
 @data_storage.route('/predicted-measurement', methods=['POST'])
-def create_predicted_measurement():
-    pm1 = request.json['pm1']
-    pm25 = request.json['pm25']
-    pm10 = request.json['pm10']
-    date = request.json['date']
-    sensor_id = request.json['sensor_id']
-    predicted_measurement = PredictedMeasurement(pm1, pm25, pm10, date, sensor_id)
-    db.session.add(predicted_measurement)
-    db.session.commit()
-    return format_predicted_measurement(predicted_measurement)
+def add_predicted_measurement_route():
+    predicted_measurement = add_predicted_measurement(
+        request.json.get('pm1'),
+        request.json.get('pm25'),
+        request.json.get('pm10'),
+        request.json.get('date'),
+        request.json.get('sensor_id')
+    )
+    return {"added_predicted_measurement": predicted_measurement.to_json()}
 
 
 # get all predicted measurements
 @data_storage.route('/predicted-measurements', methods=['GET'])
-def get_predicted_measurements():
-    predicted_measurements = PredictedMeasurement.query.order_by(PredictedMeasurement.id.asc()).all()
-    predicted_measurements_list = []
-    for predicted_measurement in predicted_measurements:
-        predicted_measurements_list.append(format_predicted_measurement(predicted_measurement))
-    return {"predicted_measurements": predicted_measurements_list}
+def get_predicted_measurements_route():
+    predicted_measurements = get_all_predicted_measurements()
+    return {"predicted_measurements": list(map(
+        lambda predicted_measurement: predicted_measurement.to_json(),
+        predicted_measurements
+    ))}
 
 
-# get single predicted measurement
-@data_storage.route('/predicted-measurement/<id>', methods=['GET'])
-def get_predicted_measurement(id):
-    predicted_measurement = PredictedMeasurement.query.filter_by(id=id).one()
-    formatted_measurement = format_predicted_measurement(predicted_measurement)
-    return {"measurement": formatted_measurement}
+# get predicted measurement by id
+@data_storage.route('/predicted-measurement/<measurement_id>', methods=['GET'])
+def get_predicted_measurement_route(measurement_id: int):
+    predicted_measurement = get_predicted_measurement(measurement_id)
+    return {"predicted_measurement": predicted_measurement.to_json()}
 
 
-# delete predicted measurement
-@data_storage.route('/predicted-measurement/<id>', methods=['DELETE'])
-def delete_predicted_measurement(id):
-    predicted_measurement = PredictedMeasurement.query.filter_by(id=id).one()
-    db.session.delete(predicted_measurement)
-    db.session.commit()
-    return f'Predicted measurement (id {id}) deleted!'
+# delete predicted measurement by id
+@data_storage.route('/predicted-measurement/<measurement_id>', methods=['DELETE'])
+def delete_predicted_measurement_route(measurement_id: int):
+    predicted_measurement = delete_predicted_measurement(measurement_id)
+    return {"deleted_predicted_measurement": predicted_measurement.to_json()}
 
 
-# update predicted measurement
-@data_storage.route('/predicted-measurement/<id>', methods=['PUT'])
-def update_predicted_measurement(id):
-    predicted_measurement = PredictedMeasurement.query.filter_by(id=id)
-    pm1 = request.json['pm1']
-    pm25 = request.json['pm25']
-    pm10 = request.json['pm10']
-    date = request.json['date']
-    sensor_id = request.json['sensor_id']
-    predicted_measurement.update(dict(pm1=pm1, pm25=pm25, pm10=pm10, date=date, sensor_id=sensor_id))
-    db.session.commit()
-    return {'measurement': format_predicted_measurement(predicted_measurement.one())}
+# update predicted measurement by id
+@data_storage.route('/predicted-measurement/<measurement_id>', methods=['PUT'])
+def update_predicted_measurement_route(measurement_id):
+    updated_predicted_measurement = update_predicted_measurement(
+        measurement_id,
+        request.json.get('pm1'),
+        request.json.get('pm25'),
+        request.json.get('pm10'),
+        request.json.get('date'),
+        request.json.get('sensor_id')
+    )
+    return {"updated_predicted_measurement": updated_predicted_measurement.to_json()}
 
 
-#   Weather measurement
-
-def format_weather_measurement(weather_measurement):
-    return {
-        "id": weather_measurement.id,
-        "temperature": weather_measurement.temperature,
-        "humidity": weather_measurement.humidity,
-        "pressure": weather_measurement.pressure,
-        "windSpeed": weather_measurement.windSpeed,
-        "windDirection": weather_measurement.windDirection,
-        "rainLevel": weather_measurement.rainLevel,
-        "date": weather_measurement.date,
-        "sensor_id": weather_measurement.sensor_id,
-    }
-
+#       Weather measurement
 
 # create a weather measurement
 @data_storage.route('/weather-measurement', methods=['POST'])
-def create_weather_measurement():
-    temperature = request.json['temperature']
-    humidity = request.json['humidity']
-    pressure = request.json['pressure']
-    windSpeed = request.json['windSpeed']
-    windDirection = request.json['windDirection']
-    rainLevel = request.json['rainLevel']
-    date = request.json['date']
-    sensor_id = request.json['sensor_id']
-    weather_measurement = WeatherMeasurement(temperature, humidity, pressure, windSpeed, windDirection, rainLevel, date,
-                                             sensor_id)
-    db.session.add(weather_measurement)
-    db.session.commit()
-    return format_weather_measurement(weather_measurement)
+def add_weather_measurement_route():
+    weather_measurement = add_weather_measurement(
+        request.json.get('temperature'),
+        request.json.get('humidity'),
+        request.json.get('pressure'),
+        request.json.get('windSpeed'),
+        request.json.get('windDirection'),
+        request.json.get('rainLevel'),
+        request.json.get('date'),
+        request.json.get('sensor_id')
+    )
+    return {"added_weather_measurement": weather_measurement.to_json()}
 
 
 # get all weather measurements
 @data_storage.route('/weather-measurements', methods=['GET'])
-def get_weather_measurements():
-    weather_measurements = WeatherMeasurement.query.order_by(WeatherMeasurement.id.asc()).all()
-    weather_measurements_list = []
-    for weather_measurement in weather_measurements:
-        weather_measurements_list.append(format_weather_measurement(weather_measurement))
-    return {"weather_measurements": weather_measurements_list}
+def get_weather_measurements_route():
+    weather_measurements = get_all_weather_measurements()
+    return {"weather_measurements": list(map(
+        lambda predicted_measurement: predicted_measurement.to_json(),
+        weather_measurements
+    ))}
 
 
-# get single weather measurement
-@data_storage.route('/weather-measurement/<id>', methods=['GET'])
-def get_weather_measurement(id):
-    weather_measurement = WeatherMeasurement.query.filter_by(id=id).one()
-    formatted_measurement = format_weather_measurement(weather_measurement)
-    return {"weather_measurement": formatted_measurement}
+# get single weather measurement by id
+@data_storage.route('/weather-measurement/<measurement_id>', methods=['GET'])
+def get_weather_measurement_route(measurement_id: int):
+    weather_measurement = get_weather_measurement(measurement_id)
+    return {"weather_measurement": weather_measurement.to_json()}
 
 
-# delete predicted measurement
-@data_storage.route('/weather-measurement/<id>', methods=['DELETE'])
-def delete_weather_measurement(id):
-    weather_measurement = WeatherMeasurement.query.filter_by(id=id).one()
-    db.session.delete(weather_measurement)
-    db.session.commit()
-    return f'Weather measurement (id {id}) deleted!'
+# delete predicted measurement by id
+@data_storage.route('/weather-measurement/<measurement_id>', methods=['DELETE'])
+def delete_weather_measurement_route(measurement_id: int):
+    weather_measurement = delete_weather_measurement(measurement_id)
+    return {"deleted_weather_measurement": weather_measurement.to_json()}
 
 
-# update predicted measurement
-@data_storage.route('/weather-measurement/<id>', methods=['PUT'])
-def update_weather_measurement(id):
-    weather_measurement = WeatherMeasurement.query.filter_by(id=id)
-    temperature = request.json['temperature']
-    humidity = request.json['humidity']
-    pressure = request.json['pressure']
-    windSpeed = request.json['windSpeed']
-    windDirection = request.json['windDirection']
-    rainLevel = request.json['rainLevel']
-    date = request.json['date']
-    sensor_id = request.json['sensor_id']
-    weather_measurement.update(dict(
-        temperature=temperature, humidity=humidity, pressure=pressure,
-        windSpeed=windSpeed, windDirection=windDirection, rainLevel=rainLevel,
-        date=date, sensor_id=sensor_id))
-    db.session.commit()
-    return {'weather_measurement': format_weather_measurement(weather_measurement.one())}
+# update predicted measurement by id
+@data_storage.route('/weather-measurement/<measurement_id>', methods=['PUT'])
+def update_weather_measurement_route(measurement_id: int):
+    updated_weather_measurement = update_weather_measurement(
+        measurement_id,
+        request.json.get('temperature'),
+        request.json.get('humidity'),
+        request.json.get('pressure'),
+        request.json.get('windSpeed'),
+        request.json.get('windDirection'),
+        request.json.get('rainLevel'),
+        request.json.get('date'),
+        request.json.get('sensor_id')
+    )
+    return {"updated_weather_measurement": updated_weather_measurement.to_json()}
